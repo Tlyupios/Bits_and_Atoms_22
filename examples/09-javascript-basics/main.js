e6d385d88e3e38031039b1f21674fc11626e60eb
Luis <luis.prax@icloud.com>
2022-05-11 09:27:58 +0200

    Folder 08/09

---
+++ examples/09-javascript-basics/main.js
@@ -0,0 +1,36 @@
+// Alert
+
+// Console log
+// https://javascript.info/debugging-chrome#logging
+
+// Variables
+// https://javascript.info/variables
+
+// Comparisons
+// https://javascript.info/comparison
+
+// Conditional branching: if
+// https://javascript.info/ifelse
+
+// Logical operators: || and && and !(NOT)
+// https://javascript.info/logical-operators
+
+// Loops: while and for
+// https://javascript.info/while-for
+
+// Objects
+// https://javascript.info/object
+
+// Arrays
+// https://javascript.info/array
+
+// Defining functions
+// function multiplyByTwo(number) {
+//   return number * 2;
+// }
+
+// Accessing the DOM
+// https://javascript.info/searching-elements-dom
+
+// const item = document.querySelector('[data-js="item"]');
+// console.log("item.innerHTML: ", item.innerHTML);
