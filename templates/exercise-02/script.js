ed93f544a02cdc068beab4dad85345bf63cbde7d
Luis <luis.prax@icloud.com>
2022-06-06 12:15:13 +0200

    Javascript 1

---
+++ templates/exercise-02/script.js
@@ -0,0 +1,68 @@
+//Code from this Website: https://web.dev/drag-and-drop/?gclid=Cj0KCQjwqPGUBhDwARIsANNwjV7kZXmAv7IGi1sqKAEJVa7ie6KZUCZV28KxYVUy7kwc2GUa9DksjU0aApH3EALw_wcB
+
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    var dragSrcEl = null;
+    
+    function handleDragStart(e) {
+      this.style.opacity = '0.4';
+      
+      dragSrcEl = this;
+  
+      e.dataTransfer.effectAllowed = 'move';
+      e.dataTransfer.setData('text/html', this.innerHTML);
+    }
+  
+    function handleDragOver(e) {
+      if (e.preventDefault) {
+        e.preventDefault();
+      }
+  
+      e.dataTransfer.dropEffect = 'move';
+      
+      return false;
+    }
+  
+    function handleDragEnter(e) {
+      this.classList.add('over');
+    }
+  
+    function handleDragLeave(e) {
+      this.classList.remove('over');
+    }
+  
+    function handleDrop(e) {
+      if (e.stopPropagation) {
+        e.stopPropagation(); // stops the browser from redirecting.
+      }
+      
+      if (dragSrcEl != this) {
+        dragSrcEl.innerHTML = this.innerHTML;
+        this.innerHTML = e.dataTransfer.getData('text/html');
+      }
+      
+      return false;
+    }
+  
+    function handleDragEnd(e) {
+      this.style.opacity = '1';
+      
+      items.forEach(function (item) {
+        item.classList.remove('over');
+      });
+    }
+    
+    
+    let items = document.querySelectorAll('.card .circle');
+    items.forEach(function(item) {
+      item.addEventListener('dragstart', handleDragStart, false);
+      item.addEventListener('dragenter', handleDragEnter, false);
+      item.addEventListener('dragover', handleDragOver, false);
+      item.addEventListener('dragleave', handleDragLeave, false);
+      item.addEventListener('drop', handleDrop, false);
+      item.addEventListener('dragend', handleDragEnd, false);
+    });
+  });
